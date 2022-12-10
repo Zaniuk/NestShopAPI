e2106c358ea4a2881b6d2e7d08e571a0d65a86b2
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-10 15:10:46 -0300

    create category dto

--- src/dtos/category/category.ts
+++ src/dtos/category/category.ts
@@ -1,1 +1,15 @@
-export class Category {}
+import { IsString, IsNotEmpty } from 'class-validator';
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+  @IsString()
+  @IsNotEmpty()
+  readonly image: string;
+}
+export class UpdateCategoryDto {
+  @IsString()
+  readonly name?: string;
+  @IsString()
+  readonly image?: string;
+}
