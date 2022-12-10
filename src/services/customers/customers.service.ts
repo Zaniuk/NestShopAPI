a5aecbc086611b13f1f91283f4b289b7bba3e41d
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-10 19:15:49 -0300

    create customers base service

--- src/services/customers/customers.service.ts
+++ src/services/customers/customers.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
-
+import { CreateCustomerDto } from 'src/dtos/customer/customer';
+import * as customers from '../../mock/customer/mock.data.json';
+import { Customer } from './types/customers.types';
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  private readonly customers: Customer[] = customers;
+  findAll() {
+    return this.customers;
+  }
+  findOne(id: number) {
+    return this.customers.find((customer) => customer.id === id);
+  }
+  delete(id: number) {
+    const index = this.customers.findIndex((customer) => customer.id === id);
+    this.customers.splice(index, 1);
+  }
+  create(customer: CreateCustomerDto): Customer {
+    const newCustomer = {
+      id: this.customers.length + 1,
+      ...customer,
+    };
+    this.customers.push(newCustomer);
+    return newCustomer;
+  }
+  update(id: number, customer: CreateCustomerDto): Customer {
+    const index = this.customers.findIndex((customer) => customer.id === id);
+    this.customers[index] = {
+      id,
+      ...customer,
+    };
+    return this.customers[index];
+  }
+}
