2a7766f79d78e748e0f7d2524966572a51bfe8ad
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-10 16:54:10 -0300

    create users service

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
-
+import * as users from '../../mock/user/mock.data.json';
+import { User } from './types/user.types';
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = users;
+  idCounter = this.users.length;
+  findAll() {
+    return this.users;
+  }
+  findOne(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+  create(user: User) {
+    this.users.push(user);
+  }
+  update(id: number, user: User): User {
+    const _user = this.users.find((user) => user.id === id);
+    const index = this.users.indexOf(_user);
+    user = { ...user, ..._user };
+    this.users[index] = user;
+    return user;
+  }
+}
