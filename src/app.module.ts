83c1f400f7a95ea783a80fcb0468969c1b0c2a45
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-10 21:11:41 -0300

    migrated users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BrandsController } from './controllers/brands/brands.controller';
-import { UsersController } from './controllers/users/users.controller';
-import { CategoriesController } from './controllers/categories/categories.controller';
-import { CustomersController } from './controllers/customers/customers.controller';
-import { ProductsController } from './controllers/products/products.controller';
-import { UsersService } from './services/users/users.service';
-import { ProductsService } from './services/products/products.service';
-import { CustomersService } from './services/customers/customers.service';
-import { CategoriesService } from './services/categories/categories.service';
-import { BrandsService } from './services/brands/brands.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    BrandsController,
-    UsersController,
-    CategoriesController,
-    CustomersController,
-    ProductsController,
-  ],
-  providers: [
-    AppService,
-    UsersService,
-    ProductsService,
-    CustomersService,
-    CategoriesService,
-    BrandsService,
-  ],
+  imports: [ProductsModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
