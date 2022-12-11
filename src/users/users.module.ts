ae80b7820ab77a05e3d347c322fbf8984e26663d
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-11 20:15:21 -0300

    create customers controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from 'src/products/products.module';
-import { CustomersController } from './controllers/customers/customers.controller';
+import { CustomerController } from './controllers/customers/customers.controller';
 import { UsersController } from './controllers/users/users.controller';
 import { CustomersService } from './services/customers/customers.service';
 import { UsersService } from './services/users/users.service';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [UsersController, CustomersController],
+  controllers: [UsersController, CustomerController],
   providers: [UsersService, CustomersService],
   exports: [UsersService, CustomersService],
 })
