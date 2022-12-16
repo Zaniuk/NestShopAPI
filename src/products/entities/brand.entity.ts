aa5e7088bd1c170c17fb5c43b8ec12af9952e16f
Zaniuk <geroza49@gmail.com>
2022-12-16 01:27:13 -0300

    add typeorm decorators

--- src/products/entities/brand.entity.ts
+++ src/products/entities/brand.entity.ts
@@ -1,5 +1,10 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
 export class Brand {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ type: 'varchar', length: 64, unique: true })
   name: string;
+  @Column({ type: 'varchar', length: 1024 })
   image: string;
 }
--- src/products/entities/category.entity.ts
+++ src/products/entities/category.entity.ts
@@ -1,4 +1,8 @@
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+@Entity()
 export class Category {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column({ type: 'varchar', length: 64, unique: true })
   name: string;
 }
