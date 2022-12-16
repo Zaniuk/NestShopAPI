6c9b51996fb63e53fd79d77ed8199552d45c5b5e
Zaniuk <geroza49@gmail.com>
2022-12-16 01:02:59 -0300

    create database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
+import { Client } from 'pg';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
 import { enviroments } from '../enviroments';
+import { DatabaseModule } from './database/database.module';
 import config from '../config';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -17,6 +20,7 @@ import config from '../config';
     }),
     UsersModule,
     ProductsModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import config from '../../config';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        return {
+          type: 'postgres',
+          host: configService.database.host,
+          port: configService.database.port,
+          username: configService.database.username,
+          password: configService.database.password,
+          database: configService.database.name,
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {}
