75102f4a3cf6b03852aa2cf84c84b19836c4e38d
Zaniuk <geroza49@gmail.com>
2022-12-16 01:28:25 -0300

    fixed errors post typeorm implementation

--- src/products/controllers/products/products.controller.ts
+++ src/products/controllers/products/products.controller.ts
@@ -30,13 +30,10 @@ export class ProductsController {
   @Get()
   @ApiOperation({ summary: 'List of products' })
   getProducts(
+    @Query('brand') brand: string,
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
-    @Query('brand') brand: string,
   ) {
-    // return {
-    //   message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
-    // };
     return this.productsService.findAll();
   }
 
@@ -48,28 +45,6 @@ export class ProductsController {
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', ParseIntPipe) productId: number) {
-    // response.status(200).send({
-    //   message: `product ${productId}`,
-    // });
     return this.productsService.findOne(productId);
-  }
-
-  @Post()
-  create(@Body() payload: CreateProductDto) {
-    // return {
-    //   message: 'accion de crear',
-    //   payload,
-    // };
-    return this.productsService.create(payload);
-  }
-
-  @Put(':id')
-  update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
-    return this.productsService.update(+id, payload);
-  }
-
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return this.productsService.remove(+id);
   }
 }
