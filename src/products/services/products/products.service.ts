969349388ec897e86b4cc06e3ae0154ba33e0baa
Zaniuk <geroza49@gmail.com>
2022-12-16 01:28:03 -0300

    implement typeorm in products service

--- src/products/services/products/products.service.ts
+++ src/products/services/products/products.service.ts
@@ -4,60 +4,24 @@ import {
   UpdateProductDto,
 } from 'src/products/dtos/product/product';
 import { Product } from 'src/products/entities/product.entity';
-
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class ProductsService {
-  private counterId = 1;
-  private products: Product[] = [
-    {
-      id: 1,
-      name: 'Product 1',
-      description: 'bla bla',
-      price: 122,
-      image: '',
-      stock: 12,
-    },
-  ];
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ){}
 
-  findAll() {
-    return this.products;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    const product = this.products.find((item) => item.id === id);
+  async findOne(id: number) {
+    const product = await this.productRepository.findOne({where: {id}});
     if (!product) {
       throw new NotFoundException(`Product #${id} not found`);
     }
     return product;
-  }
-
-  create(payload: CreateProductDto) {
-    console.log(payload);
-    this.counterId = this.counterId + 1;
-    const newProduct = {
-      id: this.counterId,
-      ...payload,
-    };
-    this.products.push(newProduct);
-    return newProduct;
-  }
-
-  update(id: number, payload: UpdateProductDto) {
-    const product = this.findOne(id);
-    const index = this.products.findIndex((item) => item.id === id);
-    this.products[index] = {
-      ...product,
-      ...payload,
-    };
-    return this.products[index];
-  }
-
-  remove(id: number) {
-    const index = this.products.findIndex((item) => item.id === id);
-    if (index === -1) {
-      throw new NotFoundException(`Product #${id} not found`);
-    }
-    this.products.splice(index, 1);
-    return true;
   }
 }
