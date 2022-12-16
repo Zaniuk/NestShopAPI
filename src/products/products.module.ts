bf948798690be4c6781c506a59f7b8d46b7234ec
Zaniuk <geroza49@gmail.com>
2022-12-16 01:27:36 -0300

    add typeorm features

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,8 +5,12 @@ import { ProductsController } from './controllers/products/products.controller';
 import { BrandsService } from './services/brands/brands.service';
 import { CategoriesService } from './services/categories/categories.service';
 import { ProductsService } from './services/products/products.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Category } from './entities/category.entity';
+import { Brand } from './entities/brand.entity';
 @Module({
+  imports: [TypeOrmModule.forFeature([Product, Category, Brand])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [CategoriesService, ProductsService, BrandsService],
   exports: [ProductsService],
